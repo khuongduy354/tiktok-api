b975f8d2bd055c1ead297899f55b2be86c4ded36
khuongduy354 <khuongduy354@gmail.com>
2022-04-12 15:37:35 +0700

    fix packages, global and remove auth0

--- src/config/cloudinary.ts
+++ src/config/cloudinary.ts
@@ -1,10 +1,9 @@
-import { global } from "./../../global";
 import cl from "cloudinary";
 const cloudinary = cl.v2;
 cloudinary.config({
-  cloud_name: global.CLOUDINARY_NAME,
-  api_key: global.CLOUDINARY_KEY,
-  api_secret: global.CLOUDINARY_SECRET,
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
 });
 
 export { cloudinary };
